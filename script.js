3fc182f2db5493d0499c5b933f0b26d7fcc73481
gdekare <katja.zhilevich@gmail.com>
2026-05-24 18:16:28 +0300

    Add files via upload
    add file on sites

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 const yesBtn = document.getElementById('yes');
 const gifBlock = document.getElementById('gif-block');
 const closeGif = document.getElementById('closeGif');
+const gifImg = document.getElementById('gif-img');
 
 yesBtn.addEventListener('click', () => {
   yesBtn.disabled = true;
@@ -12,6 +13,12 @@ closeGif.addEventListener('click', () => {
   gifBlock.classList.add('hidden');
 });
 
-gifBlock.querySelector('.overlay').addEventListener('click', () => {
+// Закрытие по клику на фон (overlay)
+document.querySelector('.overlay').addEventListener('click', () => {
+  gifBlock.classList.add('hidden');
+});
+
+// Закрытие по клику на саму гифку
+gifImg.addEventListener('click', () => {
   gifBlock.classList.add('hidden');
 });
